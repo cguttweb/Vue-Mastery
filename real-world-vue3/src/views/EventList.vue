aa1bf870600210f3a98dcac6a8b08d7273a9c0fe
Chloe <cugttweb@pm.me>
2022-07-24 14:32:50 +0100

    added more vents to db.json

--- real-world-vue3/src/views/EventList.vue
+++ real-world-vue3/src/views/EventList.vue
@@ -3,6 +3,11 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Events for Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link :to="{ name: 'EventList', query: { page: page - 1 } }"
+    rel="prev" 
+    v-if="page != 1">
+    </router-link>
   </div>
 </template>
 
@@ -13,6 +18,7 @@ import EventCard from '@/components/EventCard.vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
@@ -22,7 +28,8 @@ export default {
     }
   },
   created(){
-    EventService.getEvents()
+    // 2 = number per page and this.page = current page
+    EventService.getEvents(2, this.page)
     .then(resp => {
       this.events = resp.data
     })
