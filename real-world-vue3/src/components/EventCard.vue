891e867573fb9268663142aeb3c9a2f8cbbdeebe
Chloe <cugttweb@pm.me>
2022-07-24 13:28:37 +0100

    working on eventlist setup

---
+++ real-world-vue3/src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="event-card">
+   <!-- Display event info -->
+    <span>{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  border: 1px solid #333333;
+  cursor: pointer;
+  padding: 20px;
+  margin-bottom: 18px;
+  width: 250px;
+}
+
+.event-card:hover {
+  box-shadow: 4px 5px #333333;
+}
+</style>
---
+++ real-world-vue3/src/views/EventList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="events">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Events for Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data(){
+    return {
+      events: [
+      {
+        id: 1,
+        category: 'animal welfare',
+        title: 'Dog Adoption Day',
+        description: 'Find your new dog at this event',
+        location: 'Woof Town',
+        date: '19th August 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organiser: 'Kat Laydee'
+      },
+      {
+        id: 2,
+        category: 'animal welfare',
+        title: 'Dog Adoption Day',
+        description: 'Find your new dog at this event',
+        location: 'Woof Town',
+        date: '19th August 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organiser: 'Kat Laydee'
+      },
+      {
+        id: 3,
+        category: 'animal welfare',
+        title: 'Beach Cleanup',
+        description: 'Find your new dog at this event',
+        location: 'Woof Town',
+        date: '19th August 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organiser: 'Kat Laydee'
+      },
+ 
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
