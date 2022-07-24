c28abbde247e4bf96d4de1c89512da2f09b2658e
Chloe <cugttweb@pm.me>
2022-07-24 15:39:06 +0100

    programmatic navigation

--- real-world-vue3/src/views/event/Register.vue
+++ real-world-vue3/src/views/event/Register.vue
@@ -1,9 +1,22 @@
 <template>
     <p>Register for the event here</p>
+    <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-    props: ['event']
+  props: ['event'],
+  methods: {
+    register(){
+      // call to API
+      // ig registered then redirect to event details
+      this.$router.push({
+        name: 'EventDetails'
+      })
+
+      // this.$router.go(1) or this.$router.go(-1) useful for native apps without browser buttons
+      // this.$router.replace - prevent history in the browser
+    }
   }
+}
 </script>
